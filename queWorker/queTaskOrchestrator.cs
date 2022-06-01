ae4df692cf0b463b61e7b4afcb6dbb1b1eaa23b1
tristen-tooming <tristen.tooming@mankies.fi>
2022-06-01 21:37:46 +0300

    To blob per email per day json data. Json headers not working

--- queWorker/queTaskOrchestrator.cs
+++ queWorker/queTaskOrchestrator.cs
@@ -24,6 +24,10 @@ using MySql.Data.MySqlClient;
 using System.Data.Common;
 using System.Data.SqlClient;
 
+using Azure.Storage.Blobs.Models;
+using Azure.Storage.Blobs.Specialized;
+using System.Runtime.Serialization.Json;
+
 namespace queWorker
 {
     public static class queTaskOrchestrator
@@ -36,16 +40,16 @@ namespace queWorker
             var messageString = context.GetInput<string>();
             dynamic message = JsonConvert.DeserializeObject<MessagePOCO>(messageString); // Validates the message and converts field values to EmailPOCO schema
 
-            log.LogInformation($"Orchestrated message '{message.Email}'.");
             // Handle blob exists
-            // await context.CallActivityAsync<string>("queTaskOrchestrator_ToBlob", message); // Take just the message string
+            await context.CallActivityAsync<string>("queTaskOrchestrator_ToBlob", message); // Take just the message string
             var message_attributes = await context.CallActivityAsync<string>("queTaskOrchestrator_ToMySQL", message);
 
             if (message_attributes is not null)
             {
+                log.LogInformation($"Hitted items: {message_attributes}");
                 string parsed_message = $@"
                 “Congratulate!\n
-                We have received following 10 unique attributes from you: {message_attributes}\n
+                We have received following 10 unique attributes from you {message_attributes}\n
                 Best regards, Millisecond”
                 ";
                 await context.CallActivityAsync<string>("queTaskOrchestrator_parsed_ToTable", parsed_message);
@@ -60,20 +64,49 @@ namespace queWorker
         }
 
         [FunctionName("queTaskOrchestrator_ToBlob")]
-        // TODO: just pass string to the function and upload it
         public static string ToBlob(
             [ActivityTrigger] MessagePOCO message,
             [Blob("emails", FileAccess.Write, Connection = "BlobConnector")] BlobContainerClient outputContainer,
             ILogger log)
         {
+            string toBlob = null;
             log.LogInformation($"Handling account: {message.Email}");
             log.LogInformation($"Working in Blob container: {outputContainer.Name}");
-            // TODO: Log for each day and email
-            BlobClient blob = outputContainer.GetBlobClient($"{message.Email}_{message.Date}.json");
-            var content = Encoding.UTF8.GetBytes(message.Email);
+
+            // Json list. This list is used later on to add existing content if blob already exists
+            List<MessagePOCO> MessageList = new List<MessagePOCO>();
+            MessageList.Add(message);
+            BlobClient blob = outputContainer.GetBlobClient($"{message.Email}/{message.Date}.json");
+
+            log.LogInformation(blob.AccountName);
+            log.LogInformation(blob.Name);
+            log.LogInformation(blob.Uri.ToString());
+
+            // Setting blob headers (Not working here at least, throws blob does not exists error)
+            // BlobHttpHeaders headers = new BlobHttpHeaders{ContentType = "application/json"};
+            // blob.SetHttpHeaders(headers);
+
+            // Blob mangling
+            if (blob.Exists())
+            {       
+                BlobDownloadResult blobData = blob.DownloadContent();
+                string blobStringData = blobData.Content.ToString();
+                log.LogInformation(blobStringData);
+                List<MessagePOCO> blobContent = JsonConvert.DeserializeObject<List<MessagePOCO>>(blobStringData);
+                blobContent.AddRange(MessageList); // Adding message in to list
+                toBlob = JsonConvert.SerializeObject(blobContent);
+                blob.Delete(); // Only way? If blob exists we cannot upload to it. This should be in try catch block and upload old content if error
+            }
+            else
+            {
+                toBlob = JsonConvert.SerializeObject(MessageList);
+            }
+
+            // Upload to Blob Storage
+            var content = Encoding.UTF8.GetBytes(toBlob);
             using(var ms = new MemoryStream(content))
                 blob.Upload(ms);
-            
+
             return null; // Needed?
         }
 
@@ -91,36 +124,53 @@ namespace queWorker
             */
             using (var conn = new MySqlConnection(sqlConnectionString))
             {
-                log.LogInformation("Opening connection");
+                log.LogInformation("Opening MySQL connection");
                 conn.Open();
 
                 // Connection will be closed by the 'using' block
-                using (var cmd = conn.CreateCommand())
-                {   
-                    cmd.CommandText = @"tbl_insert";
-                    cmd.CommandType = CommandType.StoredProcedure;
-                    cmd.Parameters.AddWithValue("@_SenderKey", message.Key);
-                    cmd.Parameters.AddWithValue("@_Email", message.Email);
-                    cmd.Parameters.AddWithValue("@_SendDate", message.Date);
-                    cmd.Parameters.AddWithValue("@_Milliseconds", message.Milliseconds);
-                    cmd.Parameters.AddWithValue("@_EmailAttribute", "");
+                try
+                {
+                    using (var cmd = conn.CreateCommand())
+                    {   
+                        log.LogInformation($"email: {message.Email}, date: {message.Date}; Inserting item: {message.SingleAttribute}");
+                        cmd.CommandText = @"tbl_insert";
+                        cmd.CommandType = CommandType.StoredProcedure;
+                        cmd.Parameters.AddWithValue("@_SenderKey", message.Key);
+                        cmd.Parameters.AddWithValue("@_Email", message.Email);
+                        cmd.Parameters.AddWithValue("@_SendDate", message.Date);
+                        cmd.Parameters.AddWithValue("@_Milliseconds", message.Milliseconds);
+                        cmd.Parameters.AddWithValue("@_EmailAttribute", message.SingleAttribute);
 
-                    MySqlDataReader reader = cmd.ExecuteReader();
+                        MySqlDataReader reader = cmd.ExecuteReader();
 
-                    log.LogInformation($"Reader has rows: {reader.HasRows}");
-                    if (reader.HasRows)
-                    {
-                        log.LogInformation("10 email attributes send today. Processing them...");
-                        while (reader.Read())
-                            {
-                                message_attributes = message_attributes + ", " + reader.GetString(0);
-                            }
+                        if (reader.HasRows)
+                        {
+                            log.LogInformation($"email: {message.Email}, date: {message.Date}; 10 email attributes sent today. Processing them...");
+                            while (reader.Read())
+                                {
+                                    message_attributes = message_attributes + ", " + reader.GetString(0);
+                                }
+                        }
+                        else 
+                        {
+                            log.LogInformation($"email: {message.Email}, date: {message.Date}; Todays attribute count <10 or >10");
+                        }
+
                     }
-                    else 
+                }
+                // Needs better error handler
+                catch (MySqlException ex)
+                {
+                    int errorcode = ex.Number;
+                    if (errorcode != 1062) // If not duplicate data by database index
                     {
-                        log.LogInformation("Todays attribute count <10 or >10");
+                        log.LogCritical(errorcode.ToString());
+                        log.LogCritical(ex.GetBaseException().ToString());
                     }
-
+                    else
+                    {
+                        log.LogInformation($"email: {message.Email}, date: {message.Date}; item '{message.SingleAttribute}' already in database");
+                    }
                 }
             }
             return message_attributes;
