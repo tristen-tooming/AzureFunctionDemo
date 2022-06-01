1d4cb11c1389738d7f81855cea85d4407f5327a2
tristen-tooming <tristen.tooming@mankies.fi>
2022-06-01 16:01:12 +0300

    With timestamp per millisecond and Singleattribute

--- queWorker/emailPOCO.cs
+++ queWorker/emailPOCO.cs
@@ -1,15 +1,14 @@
 
-using System.Collections.Generic;
 using System;
 
-namespace queWorker
+namespace emailProcessWorker
 {
-    public class EmailPOCO
+    public class MessagePOCO
     {
         public string Key { get; set; }
         public string Email { get; set; }
-        public DateTime Date { get; set; } 
+        public string Date { get; set; } 
         public Double Milliseconds { get; set; }
-        public string Attribute { get; set; }
+        public string SingleAttribute { get; set; }
     }
 }
